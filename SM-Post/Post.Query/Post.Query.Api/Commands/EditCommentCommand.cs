349caa8c54acbdbde5e1271d7bc61dde1c921f50
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-09-07 17:36:49 +0100

    add EditCommentCommand

--- SM-Post/Post.Query/Post.Query.Api/Commands/EditCommentCommand.cs
+++ SM-Post/Post.Query/Post.Query.Api/Commands/EditCommentCommand.cs
@@ -1,6 +1,11 @@
-﻿namespace Post.Query.Api.Commands
+﻿using CQRS.Core.Commands;
+
+namespace Post.Query.Api.Commands;
+public class EditCommentCommand : BaseCommand
 {
-    public class EditCommentCommand
-    {
-    }
+    public Guid CommentId { get; set; }
+    public string Comment { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+
 }
+
