70cc2b0b1e4ef8e2d8e043edec5e24d409d96016
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-09-07 18:20:03 +0100

    add Events

---
+++ SM-Post/Post.Common/Events/PostRemovedEvent.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+public class PosttRemovedEvent : BaseEvent
+{
+    public PosttRemovedEvent() : base(nameof(PosttRemovedEvent))
+    {
+    }
+
+}
