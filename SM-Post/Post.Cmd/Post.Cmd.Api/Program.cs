8a9959ee3aaf91c26a0f46e7e92a3dc72a991c18
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-09-09 18:48:08 +0100

    Register Command Handlers

--- SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -4,9 +4,11 @@ using CQRS.Core.Infrastructure;
 using Post.Cmd.Api.Commands;
 using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Dispatchers;
 using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repository;
 using Post.Cmd.Infrastructure.Stores;
+using Post.Query.Api.Commands;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,19 @@ builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
 builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>,EventSourcingHandler>();
 builder.Services.AddScoped<ICommandHandler, CommandHandler>();
+
+var commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+var dispatcher = new CommandDispatcher();
+dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandleAsync);
+
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
