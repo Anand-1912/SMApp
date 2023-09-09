eff613634bca031eadd39eeb28feeb6d46e6bee3
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-09-09 11:16:54 +0100

    Add Aggregate

---
+++ CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,62 @@
+﻿using CQRS.Core.Events;
+using System.Reflection.Metadata;
+
+namespace CQRS.Core.Domain;
+public abstract class AggregateRoot
+{
+    protected Guid _id;
+
+    // events are used to make state changes to the aggregate
+    private readonly List<BaseEvent> _changes = new();
+
+    public Guid Id
+    {
+        get { return _id; }
+    }
+
+    // version is zero based
+    public int Version { get; set; } = -1;
+
+    public IEnumerable<BaseEvent> GetUncommitedChanges()
+    {
+        return _changes;
+    }
+   
+    public void MarkChangesAsCommitted()
+    {
+        // this will be called after committing the uncommitted changes in eventstore
+        _changes.Clear();
+    }
+
+    private void ApplyChange(BaseEvent @event, bool isNew)
+    {
+        // gets a method named 'Apply' and with paramters of type of @event (concrete type not BaseEvent)
+        var method = this.GetType().GetMethod("Apply", new Type[] { @event.GetType()});
+
+        if(method is null)
+        {
+            throw new ArgumentNullException(nameof(method),$"The Apply method is not available in the Aggregate for {@event.GetType().Name}!" );
+        }
+
+        method.Invoke(this, new object[] { @event });
+
+        if (isNew)
+        {
+            _changes.Add( @event );
+        }
+    }
+
+    protected void RaiseEvent(BaseEvent @event)
+    {
+        ApplyChange(@event, true);
+    }
+
+    // replay all the events for a given instance of an Aggregate from eventstore
+    public void ReplayEvents(IEnumerable<BaseEvent> events)
+    {
+        foreach(var @event in events)
+        {
+            ApplyChange( @event, false);
+        }
+    }
+}
