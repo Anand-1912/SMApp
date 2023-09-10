8af9c0326520d6ab9a00db2cc63141eb140c9ce9
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-09-10 21:25:57 +0100

    Add Event Consumer and ConsumerHostedService

---
+++ CQRS-ES/CQRS.Core/Consumers/IEventConsumer.cs
@@ -0,0 +1,5 @@
+﻿namespace CQRS.Core.Consumers;
+public interface IEventConsumer
+{
+    void Consume(string topic);
+}
--- SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,5 +1,8 @@
+using Confluent.Kafka;
+using CQRS.Core.Consumers;
 using Microsoft.EntityFrameworkCore;
 using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.Repositories;
@@ -22,6 +25,8 @@ dataContext.Database.EnsureCreated();
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
+builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
+builder.Services.AddScoped<IEventConsumer, EventConsumer>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
